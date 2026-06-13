811e135dd6d27dfec2841d58894da6e52827488b
Vadimka Komissarov <admin@vkom.cc>
2026-06-13 15:01:33 +0900

    add new files from submit

---
+++ 練習１/pra5_2.py
@@ -0,0 +1,11 @@
+# 入力した得点をリストにして返す
+asagi_scores = input_scores('浅木')
+matsuda_scores = input_scores('松田')
+
+# 得点リストをもとに平均点を計算
+asagi_avg = calc_average(asagi_scores)
+matsuda_avg = calc_average(matsuda_scores)
+
+# 結果を出力
+output_result('浅木',asagi_avg)
+output_result('松田',matsuda_avg)
